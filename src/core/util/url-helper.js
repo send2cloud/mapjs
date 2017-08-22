7badee5984f296985e6dbbb7e33ec116ac93436d
Gojko Adzic <gojko@gojko.com>
2017-08-22 19:08:25 +0200

    moved model files into mapjs -- module still exists

---
+++ src/core/util/url-helper.js
@@ -0,0 +1,30 @@
+/*global module*/
+const URLHelper = function () {
+	'use strict';
+	const self = this,
+		urlPattern = /(https?:\/\/|www\.)[\w-]+(\.[\w-]+)+([\w\(\)\u0080-\u00FF.,!@?^=%&amp;:\/~+#-]*[\w\(\)\u0080-\u00FF!@?^=%&amp;\/~+#-])?/i;
+
+	self.containsLink = function (text) {
+		return urlPattern.test(text);
+	};
+
+	self.getLink  = function (text) {
+		let url = text && text.match(urlPattern);
+		if (url && url[0]) {
+			url = url[0];
+			if (!/https?:\/\//i.test(url)) {
+				url = 'http://' + url;
+			}
+		}
+		return url;
+	};
+
+	self.stripLink  = function (text) {
+		if (!text) {
+			return '';
+		}
+		return text.replace(urlPattern, '').trim();
+	};
+};
+
+module.exports = new URLHelper();
