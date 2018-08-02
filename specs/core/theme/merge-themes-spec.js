bcc1d44e80f52908ba8632913d28a226e34ea2bc
David de Florinier <dave@deflorinier.me.uk>
2018-08-02 11:53:35 +0100

    merge-themes + spec

---
+++ specs/core/theme/merge-themes-spec.js
@@ -0,0 +1,104 @@
+/*global require, describe, it, expect*/
+
+const underTest = require('../../../src/core/theme/merge-themes'),
+	defaultTheme = require('../../../src/core/theme/default-theme');
+
+describe('mergeThemes', () => {
+	'use strict';
+	describe('should throw invalid-args', () => {
+		it('when theme is falsy', () => {
+			expect(() => underTest(undefined, {})).toThrowError('invalid-args');
+		});
+		it('when theme is not an object', () => {
+			expect(() => underTest(true, {})).toThrowError('invalid-args');
+		});
+		it('when themeOverride is falsy', () => {
+			expect(() => underTest(defaultTheme)).toThrowError('invalid-args');
+		});
+		it('when themeOverride is not an object', () => {
+			expect(() => underTest(defaultTheme, 'not an object')).toThrowError('invalid-args');
+		});
+	});
+	it('should return if there are no overrides', () => {
+		expect(underTest(defaultTheme, {})).toEqual(defaultTheme);
+	});
+	it('should allow overriding of connectors', () => {
+		const expected = JSON.parse(JSON.stringify(defaultTheme)),
+			override = {
+				connector: {
+					default: {
+						label: {
+							backgroundColor: 'red'
+						}
+					}
+				}
+			};
+		expected.connector.default.label.backgroundColor = 'red';
+		expect(underTest(defaultTheme, override)).toEqual(expected);
+	});
+	it('should allow overriding of links', () => {
+		const expected = JSON.parse(JSON.stringify(defaultTheme)),
+			override = {
+				link: {
+					default: {
+						label: {
+							position: {
+								ratio: 0.75
+							}
+						}
+					}
+				}
+			};
+		expected.link.default.label.position.ratio = 0.75;
+		expect(underTest(defaultTheme, override)).toEqual(expected);
+	});
+	it('should allow overriding of existing node styles', () => {
+		const expected = JSON.parse(JSON.stringify(defaultTheme)),
+			override = {
+				node: [
+					{
+						name: 'level_1',
+						text: {
+							margin: 10.0
+						}
+					},
+					{
+						name: 'activated',
+						border: {
+							line: {
+								color: 'red'
+							}
+						}
+					}
+				]
+			};
+		expected.node.find(n => n.name === 'level_1').text = {margin: 10.0};
+		expected.node.find(n => n.name === 'activated').border.line.color = 'red';
+		expect(underTest(defaultTheme, override)).toEqual(expected);
+	});
+	it('should allow adding of new node styles', () => {
+		const expected = JSON.parse(JSON.stringify(defaultTheme)),
+			override = {
+				node: [
+					{
+						name: 'level_2',
+						backgroundColor: 'red'
+					},
+					{
+						name: 'level_3',
+						backgroundColor: 'blue'
+					}
+				]
+			};
+		expected.node.push({
+			name: 'level_2',
+			backgroundColor: 'red'
+		});
+		expected.node.push({
+			name: 'level_3',
+			backgroundColor: 'blue'
+		});
+
+		expect(underTest(defaultTheme, override)).toEqual(expected);
+	});
+});
